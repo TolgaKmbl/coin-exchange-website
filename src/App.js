2045cbbf4ab472d6aec01f0ba4bed0a518587775
Tolga Kumbul <61317042+TolgaKmbl@users.noreply.github.com>
2021-06-28 17:29:56 +0300

    styled component update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Coin from './components/Coin/Coin';
+import AccountBalance from './components/AccountBalance/AccountBalance'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           Coin Exchange
         </h1>
         </header>
+        <AccountBalance amount={10000} />
         <table className="coin-table">
         <thead>
             <tr>
---
+++ src/components/AccountBalance/AccountBalance.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components'
+
+const Section = styled.section`
+    font-size: 2rem;
+    text-align: left;
+    padding: 1.5rem 0 1.5rem 5rem;
+`
+
+export default class AccountBalance extends Component {
+    render() {
+        return (
+            <Section>
+                The Balance is: ${this.props.amount}
+            </Section>
+        )
+    }
+}
+
+AccountBalance.propTypes = {   
+    amount: PropTypes.number.isRequired
+}
